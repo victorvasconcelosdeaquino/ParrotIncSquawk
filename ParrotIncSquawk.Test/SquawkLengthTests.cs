4958e3b905c11600b4e3d2fc7d6a76b7f1e4ecaf
Victor Vasconcelos de Aquino <victor.vasconcelos181@gmail.com>
2023-11-26 22:05:24 +0000

    More tests

--- ParrotIncSquawk.Test/SquawkLengthTests.cs
+++ ParrotIncSquawk.Test/SquawkLengthTests.cs
@@ -47,6 +47,29 @@ namespace ParrotIncSquawk.Test
         }
 
         [Fact]
+        public void SquawkWhenTheTextIsGreaterThan_400_ShouldFail()
+        {
+            // Arrange
+            SquawkRequest model = new()
+            {
+                Text = $"This is an example text for Squawk to test whether it is possible to exceed the " +
+                $"400 character limit in a Squawk, which should not be possible, since our application is " +
+                $"very reliable, scalable, uses good practices and our company has the best and the most " +
+                $"qualified professionals on the market. Unfortunately, with this beautiful text, we need " +
+                $"to make it fail, but it's for a good reason, it's to prove that our test is efficient.",
+            };
+            SquawkValidation validationRules = new();
+            int count = model.Text.Length;
+
+            // Act
+            ValidationResult validationResult = validationRules.Validate(model);
+
+            // Assert
+            Assert.True(count > 400);
+            Assert.False(validationResult.IsValid);
+        }
+
+        [Fact]
         public void SquawkWhenTheTextContainsWordsNotAllowed()
         {
             // Arrange
